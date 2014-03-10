6d3daabdb2ea12914d04585dae67e6cc861efa80
Ribesg <Ribesg@yahoo.fr>
2014-03-10 16:41:09 +0100

    Try!

---
+++ bot.js
@@ -0,0 +1,55 @@
+/**
+ * Just a bot to beat the default one
+ */
+
+var config = {
+	2: {
+		point: "11",
+		orientation: "vertical"
+	},
+	3: {
+		point: "31",
+		orientation: "horizontal"
+	},
+	4: {
+		point: "74",
+		orientation: "vertical"
+	},
+	5: {
+		point: "07",
+		orientation: "horizontal"
+	}
+};
+
+var input = JSON.parse(process.argv.slice(2));
+
+var moves = [
+	"00",
+	"01",
+	"22",
+	"23",
+	"24",
+	"42",
+	"43",
+	"44",
+	"45",
+	"37",
+	"47",
+	"57",
+	"67",
+	"77"
+];
+
+if (input.cmd == "init") {
+	console.log(JSON.stringify(config));
+} else {
+	var i = 0;
+	while (moves[i] in input.hit || moves[i] in input.missed) {
+		i++;
+	}
+	console.log(JSON.stringify({
+		move: moves[i]
+	}));
+}
+
+process.exit(0);
